bfadcdcad134eb26655079d88e59844593767b9d
Yuri <yuriw2001@gmail.com>
2025-08-03 07:50:40 -0400

    Initial commit

---
+++ evermeta/src/main/java/com/test/evermeta/EvermetaApplication.java
@@ -0,0 +1,13 @@
+package com.test.evermeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvermetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvermetaApplication.class, args);
+	}
+
+}
---
+++ evermeta/src/test/java/com/test/evermeta/EvermetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.evermeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvermetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
